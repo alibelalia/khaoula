8ba12913c072afb0280b32ee95ad69fc3c2050b7
Ali Belalia <89483298+alibelalia@users.noreply.github.com>
2024-08-15 01:38:32 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showMessage() {
-    const messageDiv = document.getElementById('message');
-    messageDiv.textContent = "aaa khaoula ana walftk w mn9drch n93od bla bik w dkhalty galby w rani nbghiiik w nkol d9i9a netwahcheQ";
-    messageDiv.style.display = 'block';
-}
